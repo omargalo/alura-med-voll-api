535bf4b40878003342afc366256f445b85706cb2
Omar García López <omar.garcia@omargl.net>
2024-05-20 15:13:26 -0600

    validations

--- api/src/main/java/med/voll/api/medico/DatosRegistroMedico.java
+++ api/src/main/java/med/voll/api/medico/DatosRegistroMedico.java
@@ -1,13 +1,26 @@
 package med.voll.api.medico;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import med.voll.api.direccion.DatosDireccion;
 
 // DatosRegistroMedico es nuestro DTO para recibir lo que viene de la API
 public record DatosRegistroMedico(
+        @NotBlank
         String nombre,
+        @NotBlank
+        @Email
         String email,
-        String documentos,
+        @NotBlank
+        @Pattern(regexp = "\\d{4,6}")
+        String documento,
+        @NotNull
         Especialidad especialidad,
+        @NotNull
+        @Valid
         DatosDireccion direccion
 ) {
 }
--- api/src/main/java/med/voll/api/medico/Medico.java
+++ api/src/main/java/med/voll/api/medico/Medico.java
@@ -26,4 +26,12 @@ public class Medico {
     @Embedded
     private Direccion direccion;
 
+    public Medico(DatosRegistroMedico datosRegistroMedico) {
+        this.nombre = datosRegistroMedico.nombre();
+        this.email = datosRegistroMedico.email();
+        this.documento = datosRegistroMedico.documento();
+        this.especialidad = datosRegistroMedico.especialidad();
+        this.direccion = new Direccion(datosRegistroMedico.direccion());
+
+    }
 }
