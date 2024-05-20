453517e3c91f84995b9dc47289d62523751cf016
Omar García López <omar.garcia@omargl.net>
2024-05-20 12:09:04 -0600

    connection to azure mysql database

--- api/src/main/java/med/voll/api/controller/MedicoController.java
+++ api/src/main/java/med/voll/api/controller/MedicoController.java
@@ -1,5 +1,6 @@
 package med.voll.api.controller;
 
+import med.voll.api.medico.DatosRegistroMedico;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,8 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class MedicoController {
 
     @PostMapping
-    public void registrarMedico(@RequestBody String parametro){
-        System.out.println("El request llegó correctamente.");
-        System.out.println(parametro);
+    public void registrarMedico(@RequestBody DatosRegistroMedico datosRegistroMedico){
+        System.out.println(datosRegistroMedico);
     }
 }
